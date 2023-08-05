2946fc110565d4d0c06fd41b8c3fc01792dc8655
Md Moin <developermdmoin@gmail.com>
2023-08-06 02:14:19 +0530

    react-form-validation-checker code initiated

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+type DynamicPayload = Record<
+  string,
+  string | number | boolean | object | Array<any>
+>
+
+export class validations {
+  public static main (payload: DynamicPayload = {}) {
+    for (const key in payload as DynamicPayload) {
+      if (!payload[key]) {
+        return `The ( ${key} )  field is empty.`
+      }
+      if (payload[key] == '') {
+        return `The ( ${key} )  field is empty.`
+      }
+    }
+    return true
+  }
+}
