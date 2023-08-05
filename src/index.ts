ecb8524932cf891a745f88e10d23359303e76b9c
Md Moin <developermdmoin@gmail.com>
2023-08-06 03:25:59 +0530

    veersion 6

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-type DynamicPayload = Record<
-  string,
-  string | number | boolean | object | Array<any>
->
+type DynamicPayload = Record<string, string | number | boolean | object | Array<any>>
 
-export class validations {
-    public static main (payload: DynamicPayload = {}) {
-      for (const key in payload as DynamicPayload) {
-        if (!payload[key]) {
-          return `The ( ${key} )  field is empty.`
-        }
-        if (payload[key] == '') {
-          return `The ( ${key} )  field is empty.`
-        }
-      }
-      return true
+export const validations = async (payload: DynamicPayload = {}  ) => {
+
+  for (const key in payload as DynamicPayload ) {
+    if (!payload[key]) {
+      return `The ( ${key} )  field is empty.`
+    }
+    if (payload[key] == '') {
+      return `The ( ${key} )  field is empty.`
     }
   }
+  return true 
+}
