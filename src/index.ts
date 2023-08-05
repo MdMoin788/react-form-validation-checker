830038d92237a948ee21bbfbd4037fb14c741e91
Md Moin <developermdmoin@gmail.com>
2023-08-06 03:08:11 +0530

    new code updated

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,16 @@ type DynamicPayload = Record<
   string | number | boolean | object | Array<any>
 >
 
-// export class Validations {
-//   public async  main (payload: DynamicPayload = {}) {
-//     for (const key in payload as DynamicPayload) {
-//       if (!payload[key]) {
-//         return `The ( ${key} )  field is empty.`
-//       }
-//       if (payload[key] == '') {
-//         return `The ( ${key} )  field is empty.`
-//       }
-//     }
-//     return true
-//   }
-// }
-
-
-export const reactFormValidation = async (payload: DynamicPayload = {}  ) => {
-
-  for (const key in payload as DynamicPayload ) {
-    if (!payload[key]) {
-      return `The ( ${key} )  field is empty.`
+export class Validations {
+  public static   main (payload: DynamicPayload = {}) {
+    for (const key in payload as DynamicPayload) {
+      if (!payload[key]) {
+        return `The ( ${key} )  field is empty.`
+      }
+      if (payload[key] == '') {
+        return `The ( ${key} )  field is empty.`
+      }
     }
-    if (payload[key] == '') {
-      return `The ( ${key} )  field is empty.`
-    }
+    return true
   }
-  return true 
 }
