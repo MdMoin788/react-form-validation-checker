e7b5f623c0506a70b7693a13f10b0eb7962da069
Md Moin <developermdmoin@gmail.com>
2023-08-07 11:33:32 +0530

    version 11

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,71 @@
- export type formTypes = Record<string, string | number | boolean | object | Array<any>>
+//  export type formTypes = Record<string, string | number | boolean | object | Array<any>>
 
  
 
-export const validations = async (payload: formTypes = {}  ) => {
+// export const validations = async (payload: formTypes = {}  ) => {
 
-  for (const key in payload as formTypes ) {
-    if (!payload[key]) {
+//   for (const key in payload as formTypes ) {
+//     if (!payload[key]) {
+//       return `The ( ${key} )  field is empty.`
+//     }
+//     if (payload[key] == '') {
+//       return `The ( ${key} )  field is empty.`
+//     }
+//   }
+//   return true 
+// }
+
+
+
+
+export type DynamicPayload = Record<string, string | number | boolean | object | Array<any>>
+export type optionalTypes = any
+
+ const formValidation = async (
+  payload: DynamicPayload = {},
+  optional: optionalTypes = []
+) => {
+  // const types: Record<string, string> = {};
+
+  for (const key in payload as DynamicPayload) {
+    // types[key] = typeof payload[key];
+
+    if (!payload[key] && !optional.includes(key)) {
       return `The ( ${key} )  field is empty.`
     }
-    if (payload[key] == '') {
+    if (payload[key] == '' && !optional.includes(key)) {
       return `The ( ${key} )  field is empty.`
     }
   }
-  return true 
+
+  return true
 }
+
+
+const cssClass: any = (payload: any, optional:any=[], color: DynamicPayload) => {
+  let colors: DynamicPayload = {
+    borderColor: '',
+    staus: false,
+  }
+  
+  for (const key in payload as DynamicPayload) {
+    if (!payload[key] && !optional.includes(key)) {
+      colors[key] = color
+      return colors
+    }
+    if (payload[key] == '' && !optional.includes(key)) {
+      colors[key] = color
+      return colors
+    } 
+  }
+
+  return colors
+}
+
+
+
+export const useFormValidation = {cssClass, formValidation}
+
 
 
 
